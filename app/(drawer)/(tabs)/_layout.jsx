090cce0f852f30b0800d5fd2365fc499c1b8a495
0xQan <125211961+FurqanHun@users.noreply.github.com>
2025-12-20 11:49:19 +0000

    feat: add tab icons and boilerplate for all navigation screens

--- app/(drawer)/(tabs)/_layout.jsx
+++ app/(drawer)/(tabs)/_layout.jsx
@@ -3,17 +3,42 @@ import { Ionicons } from "@expo/vector-icons";
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#FF6347", // tomato color
+        tabBarInactiveTintColor: "gray",
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Explore",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="restaurant" size={24} color={color} />
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="restaurant" size={size} color={color} />
           ),
         }}
       />
-      {/*add "search" and "vault" screens here */}
+
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="vault"
+        options={{
+          title: "My Vault",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="book" size={size} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
