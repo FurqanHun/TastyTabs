feb5ae127dcca82cffc6a2e83e79ccbbd63a51d6
0xQan <125211961+FurqanHun@users.noreply.github.com>
2025-12-21 18:40:48 +0000

    wrap root layout with QueryClientProvider

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,18 +2,23 @@ import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <Provider store={store}>
-      {/* critical wrapper for the Drawer swipe to work */}
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(drawer)" />
-          {/* we add this so [id].jsx screen is part of the stack */}
-          <Stack.Screen name="recipe/[id]" options={{ headerShown: true }} />
-        </Stack>
-      </GestureHandlerRootView>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        {/* critical wrapper for the Drawer swipe to work */}
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="(drawer)" />
+            {/* we add this so [id].jsx screen is part of the stack */}
+            <Stack.Screen name="recipe/[id]" options={{ headerShown: true }} />
+          </Stack>
+        </GestureHandlerRootView>
+      </Provider>
+    </QueryClientProvider>
   );
 }
